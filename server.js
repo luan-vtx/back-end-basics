3439ba1f783e64e112a7961323cb4fda456a8df6
Luan Xavier <luantenorio3@gmail.com>
2021-11-01 16:55:26 -0300

    build: add route get

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+  return res.send('Hello, world!');
+})
+
 const PORT = 3000;
 
 server.listen(PORT, () => {
